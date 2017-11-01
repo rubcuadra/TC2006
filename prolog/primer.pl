df30dd7ea9cbf54a6d04fb91f99535ef095330db
rubcuadra <rubcuadra@gmail.com>
2017-11-01 13:34:35 -0600

    v

--- prolog/primer.pl
+++ prolog/primer.pl
@@ -8,7 +8,7 @@ ancestor(X, Y) :- parent(X, Y).
 ancestor(X, Y) :- parent(X, Z), ancestor(Z, Y).
 
 maxL([],V,V).
-maxL([H|T],V,R) :- H>V -> maxL(T,H,R) ; maxL(T,V,R).
+maxL([H|T],V,R) :- H>V -> maxL(T,H,R) ; maxL(T,V,R). %% H>V? :
 maxLista([H|T],M) :- maxL(T,H,M).
 
 splitea(Lista,X,Y) :- append(X,Y,Lista), length(X,A), length(Y,B), A=:=B,!.
@@ -17,5 +17,5 @@ main:-
 	splitea([1,2,3,4,5,6],L1,L2),
 	write(L1),
 	write(L2),
-	maxLista([-1111,-21,-7,-9,-5],M),
+	maxLista([-1111,-21,-7,-5,-9,-5],M),
 	write(M).
