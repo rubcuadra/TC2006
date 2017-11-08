51422fb208668c8b7f0a3384b6f9c8c94efab4c7
rubcuadra <rubcuadra@gmail.com>
2017-11-08 12:58:13 -0600

    added comments

--- prolog/numeros.pl
+++ prolog/numeros.pl
@@ -1,9 +1,9 @@
+%%Comparadores, nos diran si todos los e del arreglo son mayores a un numero (o menores)
 smallers(_,[]). 
 smallers(Num,[H|T])  :- smallers(Num,T), H<Num.
-
 biggersEq(_,[]). 
 biggersEq(Num,[H|T]) :- biggersEq(Num,T), H>=Num.
-
+%%Permuta, si son smallers, si son biggers, acaba
 bg_sm(Num,L,X,Y) :- 
 	append(X,Y,L), 
 	smallers(Num,X),    
